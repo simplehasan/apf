bfc7afa843874c44e5879e16b9ccf281ad68eb2c
simplehasan <inuinsane2@gmail.com>
2023-08-15 09:31:41 +0700

    toggle dark / light theme

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,19 +1,22 @@
-import { useContext } from "react";
-import { Button } from "react-bootstrap";
+import { useContext, useEffect } from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { ThemeContext } from "../context/ThemeContext";
+import { Link, useLocation } from "react-router-dom";
+import links from "../data/links.json";
+import { BsFillCloudMoonFill, BsFillCloudSunFill } from 'react-icons/bs';
 
 
 const TopNav = () => {
     const [theme, setTheme] = useContext(ThemeContext)
+    const location = useLocation();
 
-
-    const toggleTheme = (event) => {
+    const toggleTheme = () => {
         if (theme === 'dark') {
             setTheme("light");
             document.getElementById("body").className = "bg-light text-dark"
         } else {
             setTheme("dark");
-            document.body.classList.add("bg-dark")
+            document.body.classList.className = "bg-dark text-light"
             document.getElementById("body").className = "bg-dark text-light"
         }
 
@@ -22,7 +25,51 @@ const TopNav = () => {
 
     return (
         <>
-            <Button variant="dark" onClick={toggleTheme}> Toggle Dark</Button>
+            <Navbar variant='dark' expand="lg" sticky='top' bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Link to="/" className='navbar-brand'>
+                        <img src="/pln.png" alt="app logo"
+                            className="d-inline-block align-top" />
+                        {' '}
+                        <strong>APF</strong>
+                    </Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+
+                        <Nav className='me-auto'>
+                            {
+                                links.map((link, key) => {
+                                    return (
+                                        <Link className='nav-item' to={link.path} key={key} style={{ textDecoration: "none" }}>
+                                            <div className={location.pathname === link.path ? "nav-link active" : "nav-link"}>
+                                                {link.name}
+                                            </div>
+                                        </Link>
+                                    )
+                                })
+                            }
+                        </Nav>
+                        <Navbar.Text>
+                            <Button variant="dark" onClick={toggleTheme}>
+                                {
+
+
+                                    theme === "dark" ?
+                                        <>
+                                            <BsFillCloudSunFill /> {" "}
+                                            Light
+                                        </>
+                                        :
+                                        <>
+                                            <BsFillCloudMoonFill /> {" "}
+                                            Dark
+                                        </>
+                                }
+                            </Button>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </>
     )
 }
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = (props) => {
-    const [theme, setTheme] = useState("dark");
+    const [theme, setTheme] = useState("light");
 
     return (
         <ThemeContext.Provider value={[theme, setTheme]}>
